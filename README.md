98540633b82bfa9deb3d5db426f408571bd11e75
Michael Dory <mjd@doryexmachina.com>
2011-09-18 18:00:40 -0400

    initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+#Arduino deploy!#
+
+__Note:__ This assumes you have PySerial installed, which you can snag [here](http://pyserial.sourceforge.net/)
+
---
+++ serial_reader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+serial.py
+Created by Michael Dory | doryexmachina on 2011-09-18.
+"""
+
+import serial
+import sys
+import serial
+
+arduinoPort = '/dev/tty.usbmodemfd131'
+arduinoSpeed = '9600'
+ser = serial.Serial('/dev/tty.usbmodemfd131', '9600')
+
+# this is where the magic happens
+def main(arduinoPort,arduinoSpeed,arguments=None):
+		
+	ser = serial.Serial(arduinoPort, arduinoSpeed)
+	
+	while 1:
+		ser.readline()
+	
+
+# do that thang!
+if __name__ == "__main__":
+	print '\nFiring up...\n'
+	main(arduinoPort,arduinoSpeed)
